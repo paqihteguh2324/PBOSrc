9c26dfdc658826fab28e6dd04ee38e3cf66c34b1
paqihteguh2324 <paqih.teguh.tif20@polban.ac.id>
2021-10-29 07:24:01 +0700

    week 7 beres

---
+++ week7Kasus2/Main.java
@@ -0,0 +1,21 @@
+package week7Kasus2;
+
+
+public class Main {
+	public static void main(String[] args) {
+		Shape shape = new Shape();
+		Circle circle = new Circle();
+		Rectangle rec = new Rectangle();
+		Square square = new Square();
+		
+		//printan
+		System.out.println(shape);
+		System.out.println("-----------------------");
+		System.out.println(circle);
+		System.out.println("-----------------------");
+		System.out.println(rec);
+		System.out.println("-----------------------");
+		System.out.println(square);
+		System.out.println("-----------------------");
+ 	}
+}
